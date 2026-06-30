d5e58f96b94aa2d7bc8d3d53dddb45ed37d6ec37
vegandayman <robbiebankel@gmail.com>
2026-07-01 01:16:52 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -20,14 +20,13 @@
 //  Full step-by-step instructions are in README.md.
 // ============================================================================
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCmCCH4IW37HatXNI1RRGbt12C-Wv83_CA",
+  authDomain: "pa-sparet-2026.firebaseapp.com",
+  projectId: "pa-sparet-2026",
+  storageBucket: "pa-sparet-2026.firebasestorage.app",
+  messagingSenderId: "28890647170",
+  appId: "1:28890647170:web:eedeff8d787f31806a029b",
+  measurementId: "G-X1HX1DESKC"
 };
-
-export { firebaseConfig };
